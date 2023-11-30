b68b4a258b51cd86b03a7fcb9f0b16e966ca530e
Sujit1904 <103412794+Sujit1904@users.noreply.github.com>
2023-11-30 11:18:06 +0530

    test3.py

--- test3.py
+++ test3.py
@@ -1,22 +1,65 @@
 import streamlit as st
-import pandas as pd
+import requests
+import bs4
+import os
+from google.cloud import storage
 
-# Sample data
-data = {'Name': ['John', 'Jane', 'Doe'],
-        'Age': [25, 30, 22]}
+st.title('Image Scraper')
 
-df = pd.DataFrame(data)
+name = st.text_input('Search Term:', '')
+size = st.number_input('Number of Images:', min_value=1, step=1)
 
-# Streamlit app
-st.title('Streamlit Download Example')
+if st.button('Scrape Images'):
+    if not name or size <= 0:
+        st.error('Invalid input parameters')
+    else:
+        st.info('Please wait...')
+        st.write('Downloading images...')
 
-# Display the dataframe
-st.write('Sample DataFrame:', df)
+        GOOGLE_IMAGE = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&q='
+        URL_input = GOOGLE_IMAGE + name
+        URLdata = requests.get(URL_input)
+        soup = bs4.BeautifulSoup(URLdata.text, "html.parser")
+        img = soup.find_all('img')
 
-# Create a download link for the dataframe as a CSV file
-csv_data = df.to_csv(index=False)
-st.download_button(label='Download CSV', data=csv_data, file_name='sample_data.csv', key='download_button')
+        i = 0
+        image_urls = []
 
-# Alternatively, you can use st.markdown to create a download link
-csv_link = f'<a href="data:file/csv;base64,{b64encode(csv_data.encode()).decode()}" download="sample_data.csv">Download CSV</a>'
-st.markdown(csv_link, unsafe_allow_html=True)
+        for link in img:
+            try:
+                images = link.get('src')
+                ext = images[images.rindex('.'):]
+                if ext.startswith('.png'):
+                    ext = '.png'
+                elif ext.startswith('.jpg'):
+                    ext = '.jpg'
+                elif ext.startswith('.jfif'):
+                    ext = '.jfif'
+                elif ext.startswith('.com'):
+                    ext = '.jpg'
+                elif ext.startswith('.svg'):
+                    ext = '.svg'
+                data = requests.get(images, stream=True)
+                
+                # Save the image to a local file
+                filename = f'./images/{name}/{i}{ext}'
+                with open(filename, 'wb') as file:
+                    file.write(data.content)
+
+                # Upload the image to a cloud storage bucket
+                upload_image_to_bucket(filename, f'{name}/{i}{ext}')
+
+                i += 1
+                image_urls.append(filename)
+
+                if i == size:
+                    break
+            except:
+                pass
+
+        st.success('Downloaded successfully')
+
+        # Display download links
+        st.write('Downloaded Image URLs:')
+        for i, image_url in enumerate(image_urls):
+            st.markdown(f'[Download Image {i + 1}]({image_url})', unsafe_allow_html=True)
